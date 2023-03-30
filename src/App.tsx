2515cbb16bf32ae03fe7d1624ce869c20a130325
kjkandrea <kjkandrea@gmail.com>
2023-03-30 14:18:29 +0900

    feat: vite alias 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '~/assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import '~/App.css'
+import {Header} from "~/stories/Header";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header onLogin={() => console.log('logged in')} onLogout={() => console.log('logged out')} onCreateAccount={() => console.log('created account')}/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
